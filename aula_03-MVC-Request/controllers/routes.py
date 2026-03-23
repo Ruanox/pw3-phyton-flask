b1ba635a950dec6c66293a10fbbaea9aba6fce51
RUAN GONÇALVES DE MEDEIROS LIMA <ruan.lima@e239.local>
2026-03-23 13:54:25 -0300

    Coloquei cadastro de jogos

--- aula_03-MVC-Request/controllers/routes.py
+++ aula_03-MVC-Request/controllers/routes.py
@@ -6,6 +6,7 @@ from flask import render_template,request
 def init_app(app):
     listaConsole = ['Playstation 5','Xbox one','Super Nintendo','Atari','3Ds','GameBoy','Playstation 2','Nintendo Switch 2','Playstation 3','Playstation 4','Xbox 360','Xbox Series X']
  
+    listaGames= [{'titulo' : 'The Last of us','ano' : 2013,'categoria' : 'sobrevivência', 'plataforma' : 'Playstation'}]
  
     @app.route('/')       
 
@@ -47,4 +48,13 @@ def init_app(app):
         return render_template('consoles.html',
                             console = console,
                             listaConsole = listaConsole)
+    @app.route('/cadgames',methods=['GET','POST'])
+    def cadgames():     
+        #Recebendo os dados do formulario e enviando para a página
+        #Verificando se a requisição do usuario é do metodo POST
+        if request.method == 'POST':
+            #Aqui ele ira gravar os dados na lista de jogos
+            listaGames.append({'titulo' : request.form.get('titulo'), 'ano' : request.form.get('ano'),'categoria' : request.form.get('categoria'),'plataforma' : request.form.get('plataforma')})
+            
+        return render_template('cadgames.html', listaGames = listaGames)    
         
